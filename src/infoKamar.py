f0ac34405b0331e07b17bc2c65269154c993ad37
kylaisha02 <16520404@std.stei.itb.ac.id>
2022-11-23 08:59:24 +0700

    push infoKamar

---
+++ src/infoKamar.py
@@ -0,0 +1,69 @@
+import sys
+import tkinter as tk
+# from PIL import Image, ImageTk
+import os
+from tkinter import ttk
+# import mysql.connector as mysql
+import mariadb
+
+def InfoKamar(screen):
+    global root
+    screen.destroy()
+    root = tk.Tk()
+    root.title("Informasi Kamar")
+    root.geometry('1270x690')
+    root.configure(bg='white')
+    
+    MyHotellabelTitle = tk.Label(root,text="myHotel",font=("helvetica",20,"bold"),bg="white",fg="black",width=100,anchor='w').place(x=550,y=100)
+    InformasilabelTitle = tk.Label(root,text="Informasi Kamar",font=("helvetica",10,"bold"),bg="white",fg="black",width=100,anchor='w').place(x=555,y=140)
+
+    scrollbar = tk.Scrollbar(root)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+    columns = (1,2,3,4,5,6)
+    tree = ttk.Treeview(root, height=10, columns=columns, show='headings')
+    tree.column(1,width=100)
+    tree.column(2,width=100)
+    tree.column(3,width=200)
+    tree.pack(side=tk.BOTTOM,fill=tk.Y,pady=20)
+    tree.heading(1, text='Nomor Kamar')
+    tree.heading(2, text='Tipe Kamar')
+    tree.heading(3, text='Luas Kamar')
+    tree.heading(4, text='Fasilitas')
+    tree.heading(5, text='Harga per Malam')
+    tree.heading(6, text='Status Kamar')
+
+    style = ttk.Style()
+    style.configure('Treeview',font=('helvetica,11'),background='lightgrey',foreground='black',fieldbackground='dodgerblue3',rowheight=40)
+    style.map('Treeview',background=[('selected','azure4')])
+
+    try:
+        conn = mariadb.connect(
+            user='root',
+            password='****',
+            host='localhost',
+            database='myhotel'
+        )
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+    
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM informasikamar')
+
+    i=1
+    for (nomorKamar, tipeKamar, luasKamar, fasilitas, hargaPerMalam, statusKamar) in cur:
+        tree.insert(parent='',index=i,text='',values=(nomorKamar, tipeKamar, luasKamar, fasilitas, hargaPerMalam, statusKamar))
+        i = i + 1
+
+    conn.close()
+
+    def kembaliHome():
+        from home import homescreen
+        homescreen(root)
+
+    KembaliBut = tk.Button(root,text="Kembali ke Menu Utama",command=kembaliHome).place(x=99,y=113,width=180,height=49)
+    
+    scrollbar.config(command = tree.yview)
+    # root.resizable(False, False)
+    root.mainloop()
