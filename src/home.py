7bbb8680f65e0f5e281e8caa7c95182c4e2574ad
theojustinnn <114268813+theojustinnn@users.noreply.github.com>
2022-11-23 22:09:16 +0700

    Update home.py

--- src/home.py
+++ src/home.py
@@ -1,64 +1,61 @@
 import sys
 import tkinter as tk
+# from PIL import Image, ImageTk
 import os
 from tkinter import ttk
+# import mysql.connector as mysql
 import mariadb
-from riwayat import Riwayat
-from checkOut import homeCheckOut
-from checkIn import homeCheckIn
-from menuMakanan import MenuMakanan
-from pemesananMakanan1 import PemesananMakanan
-from infoKamar import InfoKamar
+from riwayat import ClassRiwayat
+from checkOut import CheckOut
+from checkIn import CheckIn
+from infoKamar import ClassInfoKamar
+# from pemesananMakanan1 import PemesananMakanan
 
-# Inisiasi info kamar
-info_kamar = InfoKamar()
+class Home():
+    def bukaRiwayat(self):
+        riwayat_var = ClassRiwayat()
+        riwayat_var.Riwayat(screenhome)
 
-def bukaInfoKamar():
-    info_kamar.Infokamar(screenhome)
-    
-def bukaRiwayat():
-    Riwayat.lihatRiwayat(screenhome)
+    def bukaCheckOut(self):
+        checkout_var = CheckOut()
+        checkout_var.homeCheckOut(screenhome)
 
-def bukaCheckOut():
-    homeCheckOut(screenhome)
+    def bukaCheckIn(self):
+        checkin_var = CheckIn()
+        checkin_var.homeCheckIn(screenhome)
 
-def bukaCheckIn():
-    homeCheckIn(screenhome)
+    def bukaInfoKamar(self):
+        infokamar_var = ClassInfoKamar()
+        infokamar_var.Infokamar(screenhome)
 
-def bukaMenuMakanan():
-    MenuMakanan(screenhome)
+    # def bukaPemesanan(self):
+        # PemesananMakanan(screenhome)
 
-def bukaPemesananMakanan():
-    PemesananMakanan(screenhome) 
+    def homescreen(self, screen):
+        global screenhome
+        screen.destroy()
+        screenhome = tk.Tk()
+        screenhome.title("myHotel")
+        screenhome.geometry("1270x690")
+        screenhome.configure(bg="#F7F0F5")
+        
+        MyHotellabelTitle = tk.Label(screenhome,text="myHotel",font=("helvetica",20,"bold"),bg="#F7F0F5",fg="black").place(x=635,y=100,anchor="center")
+        MenuUtamalabelTitle = tk.Label(screenhome,text="Menu Utama",font=("helvetica",12,"bold"),bg="#F7F0F5",fg="black").place(x=635,y=140,anchor="center")
+        
+        #button Informasi Kamar
+        InfoKamarBut = tk.Button(screenhome,text="Informasi Kamar",font=("helvetica",10,"bold"),bg="#DECBB7",command=self.bukaInfoKamar).place(x=310,y=270,width=150,height=75)
 
-def homescreen(screen):
-    global screenhome
-    screen.destroy()
-    screenhome = tk.Tk()
-    screenhome.title("myHotel")
-    screenhome.geometry("1270x690")
-    screenhome.configure(bg="#F7F0F5")
-    
-    MyHotellabelTitle = tk.Label(screenhome,text="myHotel",font=("helvetica",20,"bold"),bg="#F7F0F5",fg="black").place(x=635,y=100,anchor="center")
-    MenuUtamalabelTitle = tk.Label(screenhome,text="Menu Utama",font=("helvetica",12,"bold"),bg="#F7F0F5",fg="black").place(x=635,y=140,anchor="center")
-    
-    #button Informasi Kamar
-    InfoKamarBut = tk.Button(screenhome,text="Informasi Kamar",font=("helvetica",10,"bold"),bg="#DECBB7",command=bukaInfoKamar).place(x=310,y=270,width=150,height=75)
+        #button Check-In
+        CheckInBut = tk.Button(screenhome,text="Check-in",font=("helvetica",10,"bold"),bg="#DECBB7",command=self.bukaCheckIn).place(x=560,y=270,width=150,height=75)
 
-    #button Check-In
-    CheckInBut = tk.Button(screenhome,text="Check-in",font=("helvetica",10,"bold"),bg="#DECBB7",command=bukaCheckIn).place(x=560,y=270,width=150,height=75)
+        #button Check Out
+        CheckOutBut = tk.Button(screenhome,text="Check-out",font=("helvetica",10,"bold"),bg="#DECBB7",command=self.bukaCheckOut).place(x=810,y=270,width=150,height=75)
 
-    #button Check Out
-    CheckOutBut = tk.Button(screenhome,text="Check-out",font=("helvetica",10,"bold"),bg="#DECBB7",command=bukaCheckOut).place(x=810,y=270,width=150,height=75)
+        #button Riwayat Kamar
+        RiwayatBut = tk.Button(screenhome,text="Riwayat Kamar",font=("helvetica",10,"bold"),bg="#DECBB7",command=self.bukaRiwayat).place(x=510,y=420,width=150,height=75,anchor="center")
+        
+        #button Pemesanan Makanan
+        # CheckOutBut = tk.Button(screenhome,text="Pemesanan Makanan",font=("helvetica",10,"bold"),bg="#DECBB7",command=self.bukaPemesanan).place(x=760,y=420,width=150,height=75,anchor="center")
 
-    #button Riwayat Kamar
-    RiwayatBut = tk.Button(screenhome,text="Riwayat Kamar",font=("helvetica",10,"bold"),bg="#DECBB7",command=bukaRiwayat).place(x=310,y=400,width=150,height=75)
-    
-    #button Menu Makanan
-    menuMakananBut = tk.Button(screenhome, text="Menu Makanan", font=("helvetica",10,"bold"), bg="#DECBB7", command=bukaMenuMakanan).place(x=560,y=400,width=150,height=75)
-    
-    #button Pemesanan Makanan
-    pemesananMakananBut = tk.Button(screenhome, text="Pemesanan Makanan", font=("helvetica",10,"bold"), bg="#DECBB7", command=bukaPemesananMakanan).place(x=810,y=400,width=150,height=75)
-    
-    screenhome.resizable(False,False)
-    screenhome.mainloop()
+        screenhome.resizable(False,False)
+        screenhome.mainloop()
