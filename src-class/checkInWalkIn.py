2283d3247ee789170b41cbb5e42bc421e104c65e
alifiyabs <alifiyabshary@gmail.com>
2022-11-29 23:16:34 +0700

    update check in walk in lagi

--- src-class/checkInWalkIn.py
+++ src-class/checkInWalkIn.py
@@ -36,9 +36,9 @@ class CheckInWalkIn():
 
         # Entry nomor kamar
         self.showTitle(screenWalkIn)
-        Label(screenWalkIn, text = "Check-In Walk In", font = ("Helvetica", 20, "bold"), bg="white").place(x = 635, y = 140,anchor="center")
+        Label(screenWalkIn, text = "Check-In Walk In", font = ("Helvetica", 20, "bold"), bg="#F7F0F5").place(x = 635, y = 140,anchor="center")
 
-        Label(screenWalkIn, text = "Nomor Kamar", font = ("Helvetica", 12, "bold"), bg="white").place(x = 500, y = 220)
+        Label(screenWalkIn, text = "Nomor Kamar", font = ("Helvetica", 12, "bold"), bg="#F7F0F5").place(x = 500, y = 220)
         noKamarWalkIn_var = Entry(screenWalkIn, textvariable = noKamarWalkIn, font=("Helvetica", 15), bg = "#DECBB7", fg = "black")
         noKamarWalkIn_var.place(x = 635, y = 250, width = 300, height = 30,anchor="n")
 
@@ -50,17 +50,12 @@ class CheckInWalkIn():
         screenWalkIn.mainloop()
 
     def verifikasiCheckInWalkIn(self):
+        # Buka koneksi dengan database mariadb
+        
+        # conn
+
         # Buka koneksi dengan database mysql
-                #try:
-        #    conn = mariadb.connect (
-        #        user = 'root',
-        #       password = '*****',
-        #        host = 'localhost',
-        #       database = 'myHotel'
-        #    )
-        #except mariadb.Error as e:
-         #   print(f"Error connecting to Mysql Platform: {e}")
-          #  sys.exit(1)
+
         try:
             conn = mysql.connector.connect (
                 user = 'root',
@@ -94,8 +89,8 @@ class CheckInWalkIn():
         screenFillData.geometry("1270x690")
         screenFillData.config(bg = "#F7F0F5")
 
-        Label(screenFillData, text = "myHotel", font = ("Helvetica", 20, "bold"), bg="white").place(x=635, y=50, anchor="center")
-        Label(screenFillData, text = "Pengisian data pengunjung", font = ("Helvetica", 15, "bold"), bg="white").place(x = 635, y = 90, anchor="center")
+        Label(screenFillData, text = "myHotel", font = ("Helvetica", 20, "bold"), bg="#F7F0F5").place(x=635, y=50, anchor="center")
+        Label(screenFillData, text = "Pengisian data pengunjung", font = ("Helvetica", 15, "bold"), bg="#F7F0F5").place(x = 635, y = 90, anchor="center")
 
         global nikPengunjungFill
         global NIKPengunjungFill_var
@@ -107,75 +102,45 @@ class CheckInWalkIn():
         namaPengunjungFill = StringVar()
 
         def backToHomeCheckIn():
-            from checkIn import CheckIn
-            CheckIn().homeCheckIn(screenFillData)
+            from checkIn import ClassCheckIn
+            ClassCheckIn().homeCheckIn(screenFillData)
 
-        def bukaKonfirmasiCheckIn2():
-            self.konfirmasiCheckIn2(screenFillData)
+        def bukaCheckInValid():
+            self.showCheckInWalkInValid(screenFillData)
 
         # Entry box NIK
-        Label(screenFillData, text = "NIK Pelanggan", font = ("Helvetica", 15, "bold"), bg="white").place(x = 485, y = 160)
+        Label(screenFillData, text = "NIK Pelanggan", font = ("Helvetica", 15, "bold"), bg="#F7F0F5").place(x = 485, y = 160)
         nikPengunjungFill_var = Entry(screenFillData, textvariable = nikPengunjungFill, font=("Helvetica", 15), bg = "light grey", fg = "black")
         nikPengunjungFill_var.place(x = 635, y = 190, width = 300, height = 30, anchor = "n")
 
         # Entry box Nama Pelanggan
-        Label(screenFillData, text = "Nama Pelanggan", font = ("Helvetica", 15, "bold"), bg="white").place(x = 485, y = 220)
+        Label(screenFillData, text = "Nama Pelanggan", font = ("Helvetica", 15, "bold"), bg="#F7F0F5").place(x = 485, y = 220)
         namaPengunjungFill_var = Entry(screenFillData, textvariable = namaPengunjungFill, font=("Helvetica", 15), bg = "light grey", fg = "black")
         namaPengunjungFill_var.place(x = 635, y = 250, width = 300, height = 30, anchor = "n")
 
         # Entry box tanggal check in
-        Label(screenFillData, text = "Tanggal Check In", font = ("Helvetica", 15, "bold"), bg="white").place(x = 380, y = 330)
+        Label(screenFillData, text = "Tanggal Check In", font = ("Helvetica", 15, "bold"), bg="#F7F0F5").place(x = 385, y = 330)
         calendarIn = Calendar(screenFillData, selectmode = 'day', date_pattern = 'yyyy-mm-dd')
         calendarIn.place(x = 485, y = 360, width = 200, height = 200, anchor = "n")
 
         # Entry box tanggal check out
-        Label(screenFillData, text = "Tanggal Check Out", font = ("Helvetica", 15, "bold"), bg="white").place(x = 780, y = 330)
+        Label(screenFillData, text = "Tanggal Check Out", font = ("Helvetica", 15, "bold"), bg="#F7F0F5").place(x = 775, y = 330)
         calendarOut = Calendar(screenFillData, selectmode = 'day', date_pattern = 'yyyy-mm-dd')
         calendarOut.place(x = 785, y = 360, width = 200, height = 200, anchor = "n")
 
         # Button next menuju verifikasi data yang sudah terinsert pada database
-        Button(screenFillData, text = "Berikutnya", font = ("Helvetica", 15, "bold"), bg="#71BC68", width = 10, height = 1, command = bukaKonfirmasiCheckIn2).place(x = 670, y = 600)
+        Button(screenFillData, text = "Berikutnya", font = ("Helvetica", 15, "bold"), bg="#71BC68", width = 10, height = 1, command = bukaCheckInValid).place(x = 670, y = 600)
         
         # Button back menuju halaman utama check out
         Button(screenFillData, text = "Batal", font = ("Helvetica", 15, "bold"), bg="#F4AB6A", width = 10, height = 1, command = backToHomeCheckIn).place(x = 485, y = 600)
 
         screenFillData.mainloop()
     
-    def konfirmasiCheckIn2(self, screenFillData):
-        global screenKonfirmasi2
-        screenFillData.destroy()
-        screenKonfirmasi2 = Tk()
-        screenKonfirmasi2.title("myHotel")
-        screenKonfirmasi2.geometry("1270x690")
-        screenKonfirmasi2.config(bg = "#F7F0F5")
-
-        self.showTitle(screenKonfirmasi2)
-        self.showSectionTitle(screenKonfirmasi2)
-
-        def backToShowBook():
-            self.checkInWalkIn(screenKonfirmasi2)
-
-        Label(screenKonfirmasi2, text = "Lanjutkan Check In?", font = ("Helvetica", 12, "bold"), bg= "#F7F0F5").place(x = 635, y = 220,anchor="center")
-
-        # Button
-        Button(screenKonfirmasi2, text = "Ya", font = ("Helvetica", 12, "bold"), bg= "#DECBB7", width=10, height=1, command= self.prosesCheckInWalk).place(x = 785, y = 290, anchor="ne")
-        Button(screenKonfirmasi2, text = "Tidak", font = ("Helvetica", 12, "bold"), bg= "#FF595E", width=10, height=1, command=backToShowBook).place(x = 485, y = 290, anchor="ne")
-
-        screenKonfirmasi2.resizable(False,False)
-        screenKonfirmasi2.mainloop()
-    
     def prosesCheckInWalk(self):
         # Koneksi ke database
-                #try:
-        #    conn = mariadb.connect (
-        #        user = 'root',
-        #       password = '*****',
-        #        host = 'localhost',
-        #       database = 'myHotel'
-        #    )
-        #except mariadb.Error as e:
-         #   print(f"Error connecting to Mysql Platform: {e}")
-          #  sys.exit(1)
+        
+        # conn
+
         try:
             conn = mysql.connector.connect (
                 user = 'root',
@@ -198,24 +163,22 @@ class CheckInWalkIn():
             conn.commit()
 
             # Insert data pengunjung baru pada tabel informasi tamu hotel
-           # cur = conn.cursor()
-           # statement = "INSERT INTO informasiTamuHotel (NIK, nomorKamar, tanggalCheckIn, tanggalCheckOut, tipeKamar, namaPengguna, durasiMenginap, statusPengunjung, totalTagihan) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            cur = conn.cursor()
+            statement = "INSERT INTO informasiTamuHotel (NIK, nomorKamar, tanggalCheckIn, tanggalCheckOut, tipeKamar, namaPengunjung, durasiMenginap, statusPengunjung, totalTagihan) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
             # Kondisi jika 
-           # if (int(noKamarWalkIn.get()) < 200 and int(noKamarWalkIn.get() >= 100)):
-             #   value = (int(nikPengunjungFill.get()), int(noKamarWalkIn.get()), calendarIn.get_date(), calendarOut.get_date(), "Single", namaPengunjungFill.get(), 1, "Check-In", 300000)
-           # elif (int(noKamarWalkIn.get()) < 300 and int(noKamarWalkIn.get()) >= 200):
-              #  value = (int(nikPengunjungFill.get()), int(noKamarWalkIn.get()), calendarIn.get_date(), calendarOut.get_date(), "Double", namaPengunjungFill.get(), 2, "Check-In", 600000)
-           # else:
-            #    value = (int(nikPengunjungFill.get()), int(noKamarWalkIn.get()), calendarIn.get_date(), calendarOut.get_date(), "Deluxe", namaPengunjungFill.get(), 3 , "Check-In", 1000000)
-            #cur.execute(statement,value)
-            # conn.commit()
+            if (int(noKamarWalkIn.get()) < 200 and int(noKamarWalkIn.get() >= 100)):
+                value = (int(nikPengunjungFill.get()), int(noKamarWalkIn.get()), calendarIn.get_date(), calendarOut.get_date(), "Single", namaPengunjungFill.get(), 1, "Check-In", 300000)
+            elif (int(noKamarWalkIn.get()) < 300 and int(noKamarWalkIn.get()) >= 200):
+                value = (int(nikPengunjungFill.get()), int(noKamarWalkIn.get()), calendarIn.get_date(), calendarOut.get_date(), "Double", namaPengunjungFill.get(), 2, "Check-In", 600000)
+            else:
+                value = (int(nikPengunjungFill.get()), int(noKamarWalkIn.get()), calendarIn.get_date(), calendarOut.get_date(), "Deluxe", namaPengunjungFill.get(), 3 , "Check-In", 1000000)
+            cur.execute(statement,value)
+            conn.commit()
         except mysql.connector.Error as e:
             print(f"Error updating or retrieving entry form database: {e}")
 
         # conn.commit()
-        self.showCheckInWalkInValid(screenKonfirmasi2)
-
-
+        self.checkInWalkInBerhasil(screenWalkInValid)
 
     def getCheckInDate():
         global tglCheckIn
@@ -260,73 +223,29 @@ class CheckInWalkIn():
         # Section Title
         self.showSectionTitle(screenWalkInValid)
 
-        # Ini rencananya mau diubah2
+        # Kembali ke homescreen
         def backToCheckIn():
             from checkIn import CheckIn
             CheckIn.homeCheckIn(screenWalkInValid)
-        
+
+        # Ke layar check in berhasil
+        def bukaCheckInWalkInBerhasil():
+            self.checkInWalkInBerhasil(screenWalkInValid)
         
         # Section Title
-        Label(screenWalkInValid, text = "Detail Pesanan Pengunjung", font = ("Helvetica", 10, "bold"), bg="white").place(x = 635, y = 180,anchor="center")
+        Label(screenWalkInValid, text = "Detail Pesanan Pengunjung", font = ("Helvetica", 10, "bold"), bg="#F7F0F5").place(x = 635, y = 180,anchor="center")
 
         # Menampilkan data check in pengunjung yang valid
         self.validateCheckInWalkIn()
 
         # Button next menuju konfirmasi check in
-        Button(screenWalkInValid, text = "Berikutnya", font = ("Helvetica", 12, "bold"), bg="#DECBB7", width = 10, height = 1, command = self.insertToDatabase).place(x = 785, y = 600,anchor="ne")
+        Button(screenWalkInValid, text = "Lanjutkan", font = ("Helvetica", 12, "bold"), bg="#DECBB7", width = 10, height = 1, command = self.prosesCheckInWalk).place(x = 785, y = 600,anchor="ne")
         
         # Button back menuju halaman utama check in
         Button(screenWalkInValid, text = "Kembali", font = ("Helvetica", 12, "bold"), bg="#8F857D", width = 10, height = 1, command = backToCheckIn).place(x = 485, y = 600,anchor="ne")
 
+        screenWalkInValid.resizable(FALSE,FALSE)
         screenWalkInValid.mainloop()
-
-    def insertToDatabase(self):
-
-        # Connect to database
-
-        # Buka koneksi dengan database mysql
-                #try:
-        #    conn = mariadb.connect (
-        #        user = 'root',
-        #       password = '*****',
-        #        host = 'localhost',
-        #       database = 'myHotel'
-        #    )
-        #except mariadb.Error as e:
-         #   print(f"Error connecting to Mysql Platform: {e}")
-          #  sys.exit(1)
-        
-        try:
-            conn = mysql.connector.connect (
-                user = 'root',
-                password = 'Bel2022Fiy@',
-                host = 'localhost',
-                database = 'myHotel',
-            )
-        except mysql.connector.Error as e:
-            print(f"Error connecting to Mysql Platform: {e}")
-            sys.exit(1)
-        
-        cur = conn.cursor()
-        
-        try:
-
-            # Insert data pengunjung baru pada tabel informasi tamu hotel
-            cur = conn.cursor()
-            statement = "INSERT INTO informasiTamuHotel (NIK, nomorKamar, tanggalCheckIn, tanggalCheckOut, tipeKamar, namaPengguna, durasiMenginap, statusPengunjung, totalTagihan) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-            # Kondisi jika 
-            if (int(noKamarWalkIn.get()) < 200 and int(noKamarWalkIn.get() >= 100)):
-                value = (int(nikPengunjungFill.get()), int(noKamarWalkIn.get()), calendarIn.get_date(), calendarOut.get_date(), "Single", namaPengunjungFill.get(), 1, "Check-In", 300000)
-            elif (int(noKamarWalkIn.get()) < 300 and int(noKamarWalkIn.get()) >= 200):
-                value = (int(nikPengunjungFill.get()), int(noKamarWalkIn.get()), calendarIn.get_date(), calendarOut.get_date(), "Double", namaPengunjungFill.get(), 2, "Check-In", 600000)
-            else:
-                value = (int(nikPengunjungFill.get()), int(noKamarWalkIn.get()), calendarIn.get_date(), calendarOut.get_date(), "Deluxe", namaPengunjungFill.get(), 3 , "Check-In", 1000000)
-            cur.execute(statement,value)
-        except mysql.connector.Error as e:
-            print(f"Error updating or retrieving entry form database: {e}")
-
-        conn.commit()
-        self.checkInWalkInBerhasil(screenWalkInValid)
         
     def checkInWalkInBerhasil(self, screenWalkInValid):
         global screenWalkInBerhasil
@@ -349,7 +268,7 @@ class CheckInWalkIn():
         Label(screenWalkInBerhasil, text = "Check In Berhasil dilakukan!", font = ("Helvetica", 12, "bold"), bg= "#F7F0F5").place(x = 635, y = 220,anchor="center")
 
         # Button
-        Button(screenWalkInBerhasil, text = "Selesai Check In", font = ("Helvetica", 12, "bold"), bg= "#DECBB7", width=10, height=1, command=backToHomescreen).place(x = 785, y = 320, anchor="ne")
+        Button(screenWalkInBerhasil, text = "Selesai Check In", font = ("Helvetica", 12, "bold"), bg= "#DECBB7", width=15, height=1, command=backToHomescreen).place(x = 785, y = 320, anchor="ne")
         Button(screenWalkInBerhasil, text = "Kembali", font = ("Helvetica", 12, "bold"), bg= "#FF595E", width=10, height=1, command=ulangCheckInWalk).place(x = 485, y = 320)
 
         screenWalkInBerhasil.resizable(False,False)
